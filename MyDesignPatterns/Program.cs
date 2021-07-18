726e58728bd9d648d24f426a13d62be9dcf83c20
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-18 06:54:43 +0200

    Singleton added

---
+++ MyDesignPatterns/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using MyDesignPatterns.Singleton;
+
+namespace MyDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var singleton = new SingletonRun();
+        }
+    }
+}
