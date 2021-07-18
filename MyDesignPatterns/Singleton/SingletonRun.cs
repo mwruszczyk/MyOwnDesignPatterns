17468a790f49bb65a157f8dad1d83bfbeded6ffa
mwruszczyk <marcin.ruszczyk@gazeta.pl>
2021-07-18 06:57:15 +0200

    Singleton complete

--- MyDesignPatterns/Singleton/SingletonRun.cs
+++ MyDesignPatterns/Singleton/SingletonRun.cs
@@ -6,5 +6,11 @@ namespace MyDesignPatterns.Singleton
 {
     class SingletonRun
     {
+        public SingletonRun()
+        {
+            UpTimeClock u1 = UpTimeClock.GetUptimeClock();
+            UpTimeClock u2 = UpTimeClock.GetUptimeClock();
+            Console.ReadKey();
+        }
     }
 }
--- MyDesignPatterns/Singleton/UpTimeClock.cs
+++ MyDesignPatterns/Singleton/UpTimeClock.cs
@@ -4,7 +4,37 @@ using System.Text;
 
 namespace MyDesignPatterns.Singleton
 {
-    class Uptime
+    public class UpTimeClock
     {
+        static UpTimeClock instance;
+
+        private static object locker = new object();
+
+        public UpTimeClock()
+        {
+            DateTime saveNow = DateTime.Now;
+            Console.WriteLine("Service is working since:");
+            Console.WriteLine(saveNow);
+
+        }
+
+        public static UpTimeClock GetUptimeClock()
+        {
+
+            if (instance == null)
+            {
+                lock (locker)
+                {
+                    if (instance == null)
+                    {
+                        instance = new UpTimeClock();
+                    }
+                }
+            }
+
+
+            return instance;
+        }
+
     }
 }
